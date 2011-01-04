0f423ee616ab2ac09216c891b3e4205acbf70058
Pradeep Ranganathan <rpradeep@umich.edu>
2011-01-04 14:09:48 -0500

    Fix package declaration

--- java/src/maxconv/BinaryMaxConv.java
+++ java/src/maxconv/BinaryMaxConv.java
@@ -1,3 +1,5 @@
+package maxconv;
+
 public class BinaryMaxConv
 {
     public static void main(String args[])
--- java/src/maxconv/MaxConv.java
+++ java/src/maxconv/MaxConv.java
@@ -1,3 +1,5 @@
+package maxconv;
+
 public class MaxConv
 {
     public static void main(String args[])
